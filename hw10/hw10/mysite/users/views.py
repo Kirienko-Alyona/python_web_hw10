48965fae5436a65a8eea99bb4266bf75f5810553
Kirienko-Alyona <ladybird.miami@gmail.com>
2023-03-28 02:37:15 +0300

    cute site

--- hw10/hw10/mysite/users/views.py
+++ hw10/hw10/mysite/users/views.py
@@ -18,7 +18,7 @@ class RegisterView(View):
         return super(RegisterView, self).dispatch(request, * args, ** kwargs)
 
     def get(self, request):
-        return render(request, self.template_name, {"form": self.form_class})
+        return render(request, self.template_name, context={'title': 'My Site', "form": self.form_class})
 
     def post(self, request):
         form = self.form_class(request.POST)
@@ -28,5 +28,5 @@ class RegisterView(View):
             username = form.cleaned_data["username"]
             messages.success(request, f'Вітаю {username}. Ваш аккаунт успішно створено')
             return redirect(to="users:login")
-        return render(request, self.template_name, {"form": form})
+        return render(request, self.template_name, context={'title': 'My Site', "form": form})
     
